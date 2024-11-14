5eacab01d06a5a3e29c84695a0366bf366471f7b
m-salman-05 <90322191+m-salman-05@users.noreply.github.com>
2024-11-14 16:41:57 +0500

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-g++ ../src/*.cpp -I../include/ -L../libs/SFML/ -lsfml-graphics -lsfml-window -lsfml-system -pedantic-errors -Wall
+g++ -o animation ../src/*.cpp -I../include/ -L../libs/SFML/ -lsfml-graphics -lsfml-window -lsfml-system -pedantic-errors -Wall
